3ae4163f13e76d90ff9d26dd93b360c6b5239c55
Steve Do <hoangflinhddo@gmail.com>
2023-07-14 18:22:54 +0700

    Trell message chat

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ auth, children }) => {
+  if (!auth) {
+    return <Navigate to={"/login"} />;
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;
